cbb9e9b06a237acae3a69f4c6128386558fd4c16
kkerwqss <banykinaks@yandex.ru>
2024-02-24 14:52:33 +0300

    mistakes

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,49 +2,39 @@
 #include <cstdint>
 #include "alg.h"
 
-bool checkPrime(uint64_t value)
-{
-    if (value < 2)
-    {
+bool checkPrime(uint64_t value) {
+    if (value < 2) {
         return false;
     }
-    for (int i = 2; i * i <= value; i++)
-    {
+    for (int i = 2; i * i <= value; i++) {
         if (value % i == 0)
             return false;
     }
     return true;
 }
 
-uint64_t nPrime(uint64_t n)
-{
+uint64_t nPrime(uint64_t n) {
     int count = 0, value = 1;
-    while (count < n)
-    {
+    while (count < n) {
         value++;
-        if (checkPrime(value))
-        {
+        if (checkPrime(value)) {
             count++;
         }
     }
     return value;
 }
 
-uint64_t nextPrime(uint64_t value)
-{
+uint64_t nextPrime(uint64_t value) {
     int next = value + 1;
-    while (!checkPrime(next))
-    {
+    while (!checkPrime(next)) {
         next++;
     }
     return next;
 }
 
-uint64_t sumPrime(uint64_t hbound)
-{
+uint64_t sumPrime(uint64_t hbound) {
     uint64_t sum = 0;
-    for (int i = 2; i < hbound; i++)
-    {
+    for (int i = 2; i < hbound; i++) {
         if (checkPrime(i))
             sum += i;
     }
