772912cc00f5bec5e304445389e8e8b776ec9cbd
kkerwqss <banykinaks@yandex.ru>
2024-02-24 14:20:15 +0300

    implementing algorithm

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,40 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+	if (value < 2) {
+		return false;
+	}
+	for (int i = 2; i * i <= value; i++) {
+		if (value % i == 0)
+			return false;
+	}
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+	int count = 0, value = 1;
+	while (count < n) {
+		value++;
+		if (checkPrime(value)) {
+			count++;
+		}
+	}
+	return value;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+	int next = value + 1;
+	while (!checkPrime(next)) {
+		next++;
+	}
+	return next;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+	uint64_t sum = 0;
+	for (int i = 2; i < hbound; i++) {
+		if (checkPrime(i))
+			sum += i;
+	}
+	return sum;
 }
